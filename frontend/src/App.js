f3ee4d63b83cb0fc489178ea0a0289e2f7a94dc6
Davidvera27 <davidrestrepovera@gmail.com>
2024-11-23 22:21:01 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import PrivateRoute from "./components/PrivateRoute"; // Importar PrivateRoute
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
