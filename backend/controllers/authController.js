02049562afb3ace8e5931322e4541d3ef990f9d1
Davidvera27 <davidrestrepovera@gmail.com>
2024-11-23 22:09:03 -0500

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,29 +23,22 @@ exports.registerUser = async (req, res) => {
 exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
   
-    // Verifica si email o password no están definidos
-    if (!email || !password) {
-      return res.status(400).json({ message: "Correo y contraseña son obligatorios" });
-    }
-  
     try {
-      // Busca el usuario por email
       const user = await User.findOne({ email });
       if (!user) {
-        return res.status(400).json({ message: "Usuario no encontrado" });
+        return res.status(400).json({ message: "Credenciales inválidas" });
       }
   
-      // Compara las contraseñas
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res.status(400).json({ message: "Contraseña incorrecta" });
+        return res.status(400).json({ message: "Credenciales inválidas" });
       }
   
-      // Genera un token JWT
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
       res.status(200).json({ message: "Inicio de sesión exitoso", token });
     } catch (error) {
-      console.error("Error en loginUser:", error); // Registra cualquier error en los logs del backend
+      console.error("Error en loginUser:", error);
       res.status(500).json({ message: "Error en el servidor", error });
     }
   };
+  
