14cd85127cf623839bb21d5d0fdaccaa59512ef6
Davidvera27 <davidrestrepovera@gmail.com>
2024-11-24 22:20:52 -0500

    Update Patient.sql

--- backend/Database/Patient.sql
+++ backend/Database/Patient.sql
@@ -1,4 +1,16 @@
-CREATE TRIGGER set_updatedAt
-BEFORE UPDATE ON patients
-FOR EACH ROW
-EXECUTE FUNCTION update_updatedAt_column();
+DROP TABLE IF EXISTS PatientsGuardian;
+
+CREATE TABLE PatientsGuardian (
+    id SERIAL PRIMARY KEY,
+    id_usuario VARCHAR(255) NOT NULL,
+    parentesco VARCHAR(255) NOT NULL,
+    vive BOOLEAN NOT NULL,
+    n_documento VARCHAR(255) UNIQUE NOT NULL,
+    nombres VARCHAR(255) NOT NULL,
+    apellidos VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    telefono VARCHAR(50),
+    correo_electronico VARCHAR(255),
+    marcadores JSON,
+    FOREIGN KEY (id_usuario) REFERENCES Patients (id_usuario) ON DELETE CASCADE
+);
