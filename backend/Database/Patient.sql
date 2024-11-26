b94a9ec8914200678bd829b7cfda54868966f2c6
Davidvera27 <davidrestrepovera@gmail.com>
2024-11-25 19:38:01 -0500

    Update Patient.sql

--- backend/Database/Patient.sql
+++ backend/Database/Patient.sql
@@ -1,16 +1,1 @@
-DROP TABLE IF EXISTS PatientsGuardian;
-
-CREATE TABLE PatientsGuardian (
-    id SERIAL PRIMARY KEY,
-    id_usuario VARCHAR(255) NOT NULL,
-    parentesco VARCHAR(255) NOT NULL,
-    vive BOOLEAN NOT NULL,
-    n_documento VARCHAR(255) UNIQUE NOT NULL,
-    nombres VARCHAR(255) NOT NULL,
-    apellidos VARCHAR(255) NOT NULL,
-    direccion VARCHAR(255) NOT NULL,
-    telefono VARCHAR(50),
-    correo_electronico VARCHAR(255),
-    marcadores JSON,
-    FOREIGN KEY (id_usuario) REFERENCES Patients (id_usuario) ON DELETE CASCADE
-);
+SELECT * FROM "Patients" WHERE id_usuario = 'USR-1234567890';
