cb3b7540f03c2348d203a176c89d51eff2ccd122
Davidvera27 <davidrestrepovera@gmail.com>
2024-12-16 21:42:47 -0500

    Update patientModel.js

--- backend/models/patientModel.js
+++ backend/models/patientModel.js
@@ -1,70 +1,73 @@
 const { DataTypes } = require("sequelize");
-const { sequelize } = require("../db");
+const { sequelize } = require("../db"); // Importa conexión de PostgreSQL
 
-const Patient = sequelize.define("Patient", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Patient = sequelize.define(
+  "Patient",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    tipo_usuario: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    n_documento: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^[a-zA-Z0-9]+$/, // Solo letras y números
+      },
+    },
+    apellidos: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    nombres: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    genero: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    fecha_nacimiento: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    estado_civil: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    ocupacion: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    fotografia: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    direccion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING, // Cambiado a STRING para almacenar números telefónicos completos
+      allowNull: false,
+    },
+    correo_electronico: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
   },
-  id_usuario: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  tipo_usuario: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  n_documento: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  apellidos: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  nombres: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  genero: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  fecha_nacimiento: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-  },
-  estado_civil: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  ocupacion: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  fotografia: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  direccion: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  telefono: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  correo_electronico: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-});
+  {
+    tableName: "Patients",
+    timestamps: false,
+  }
+);
 
 module.exports = Patient;
